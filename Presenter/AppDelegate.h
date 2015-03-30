2335b665e5de0534112d47625b68c81f337e1c3c
dcatteeu <davidcatteeuw@gmail.com>
2015-03-30 17:30:53 +0200

    Initial Commit

---
+++ Presenter/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Presenter
+//
+//  Created by David Catteeuw on 30/03/15.
+//  Copyright (c) 2015 David R. Catteeuw. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Presenter/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Presenter
+//
+//  Created by David Catteeuw on 30/03/15.
+//  Copyright (c) 2015 David R. Catteeuw. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
